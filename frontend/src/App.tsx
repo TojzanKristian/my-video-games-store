9bec79e6bd49b4b221eae861353928d2436487be
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-01 10:21:42 +0200

    Google authentication and route settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Registration from './pages/Registration/Registration';
+import Login from './pages/Login/Login';
+import { googleClientId } from './config';
+import { gapi } from 'gapi-script';
 
 const App: React.FC = () => {
+  // Funkcija za podešavanje za google autentifikaciju
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: googleClientId,
+        scope: ""
+      })
+    };
+    gapi.load('client:auth2', start);
+  });
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
       </Routes>
     </Router>
   );
