049d1c5468b89ad4e71a38fa97c78a35251bedd4
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-01 10:18:19 +0200

    IUser interface

---
+++ frontend/src/interfaces/IUser.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    firstName: string;
+    lastName: string;
+    userName: string;
+    email: string;
+    password: string;
+    dateOfBirth: string;
+    country: string;
+    phoneNumber: string
+}
