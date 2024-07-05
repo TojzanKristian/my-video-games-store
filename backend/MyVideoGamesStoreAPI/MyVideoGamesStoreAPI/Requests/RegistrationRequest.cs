50295f5aee3690c308d026d99b90a19bb62a92db
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-05 14:14:11 +0200

    Request model for registration

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Requests/RegistrationRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace MyVideoGamesStoreAPI.Requests
+{
+    public class RegistrationRequest
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? Country { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
