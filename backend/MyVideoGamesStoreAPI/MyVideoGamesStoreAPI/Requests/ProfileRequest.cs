2eaaf3bae34cab3baf1046b96729fd8d9a95b70c
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-07 10:20:45 +0200

    Request model for profile

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Requests/ProfileRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace MyVideoGamesStoreAPI.Requests
+{
+    public class ProfileRequest
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? OldPassword { get; set; }
+        public string? NewPassword { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? Country { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
