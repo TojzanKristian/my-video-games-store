0807133655243ea72d19961d75f832cd7e34a514
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-08 09:53:26 +0200

    UsersDbContext

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Database/Users/UsersDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVideoGamesStoreAPI.Models;
+
+namespace MyVideoGamesStoreAPI.Database.Users
+{
+    public class UsersDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UsersDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
