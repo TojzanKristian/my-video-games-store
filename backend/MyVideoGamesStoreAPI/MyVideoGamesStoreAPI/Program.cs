b7ec66d3b3b5cc45e4dcdab3fb73633d20b83b87
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-08 09:54:39 +0200

    Database configuration with EntityFrameworkCore

--- backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Program.cs
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using MyVideoGamesStoreAPI.Database.Users;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllers();
 
 // CORS settings
@@ -14,13 +17,21 @@ builder.Services.AddCors(options =>
     });
 });
 
+// Database configuration
+builder.Services.AddDbContext<UsersDbContext>(options =>
+{
+    options.UseSqlServer("Server=KRISZTIAN\\SQLEXPRESS;Database=my-video-games-store;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=false;");
+});
+
+builder.Services.AddScoped<UsersRepository>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
