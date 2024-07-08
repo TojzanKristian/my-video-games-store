30eddaab5b893e028a4fd564b0fb9bd06d0f56a4
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-08 09:52:57 +0200

    User model

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Models/User.cs
@@ -0,0 +1,31 @@
+﻿namespace MyVideoGamesStoreAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? Country { get; set; }
+        public string? PhoneNumber { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string? firstName, string? lastName, string? userName, string? email, string? password, string? dateOfBirth, string? country, string? phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            UserName = userName;
+            Email = email;
+            Password = password;
+            DateOfBirth = dateOfBirth;
+            Country = country;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
