687d8d8423d91ba2fbe0366299c2d084afaccfe2
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-09 10:43:33 +0200

    AuthenticationResponse

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Responses/AuthenticationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVideoGamesStoreAPI.Responses
+{
+    public class AuthenticationResponse
+    {
+        public Tuple<int, string> NotFound { get; } = Tuple.Create(-2, "Nemate nalog! Molimo vas registrujte se.");
+        public Tuple<int, string> IncorrectPassword { get; } = Tuple.Create(-1, "Pogrešna lozinka!");
+        public Tuple<int, string> Authenticated { get; } = Tuple.Create(0, "Uspešno ste se prijavili!");
+    }
+}
