9d5465bd64b11f2f6d3f780a925b33e7d193d3f1
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-09 10:43:05 +0200

    GoogleAccResponse

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Responses/GoogleAccResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MyVideoGamesStoreAPI.Responses
+{
+    public class GoogleAccResponse
+    {
+        public Tuple<int, string> NewSserAddedToDB { get; } = Tuple.Create(0, "Uspešna registracija sa Google nalogom!");
+        public Tuple<int, string> LoginSuccess { get; } = Tuple.Create(1, "Uspešna prijava sa Google nalogom!");
+    }
+}
