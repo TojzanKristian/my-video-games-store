d25e1d98a75efa9b834280f86030fe2db14c5812
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-09 10:41:59 +0200

    RegistrationResponse

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Responses/RegistrationResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVideoGamesStoreAPI.Responses
+{
+    public class RegistrationResponse
+    {
+        public Tuple<int, string> AlreadyExistsWithThisUsername { get; } = Tuple.Create(-2, "Već postoji korisnik sa ovim korisničkim imenom!");
+        public Tuple<int, string> AlreadyExistsWithThisEmail { get; } = Tuple.Create(-1, "Već postoji korisnik sa ovim emailom!");
+        public Tuple<int, string> Success { get; } = Tuple.Create(0, "Uspešna registracija!");
+    }
+}
