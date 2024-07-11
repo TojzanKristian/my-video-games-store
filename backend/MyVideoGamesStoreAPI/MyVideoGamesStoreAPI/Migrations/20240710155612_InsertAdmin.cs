957b20510c15078eba8cd7faf478962a478c57b5
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-11 09:42:46 +0200

    Add InsertAdmin migration

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Migrations/20240710155612_InsertAdmin.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyVideoGamesStoreAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FirstName", "LastName", "UserName", "Email", "Password", "DateOfBirth", "Country", "PhoneNumber" },
+                values: new object[] { 1, "Kristian", "Tojzan", "TKStoreAdmin", "kristiantojzan@gmail.com", "8d9191a6a98ecc2154b73c041a74f6d5834c47973f9a0a68074454ccafbaf035", "2000-02-06", "Serbia", "0628868123" }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(table: "Users", keyColumn: "Id", keyValue: 1);
+        }
+    }
+}
