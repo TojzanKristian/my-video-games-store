a4e2a1f923e039c80626f3f6cc6600bfd1f71658
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-11 09:48:50 +0200

    EditProfileResponse

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Responses/EditProfileResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVideoGamesStoreAPI.Responses
+{
+    public class EditProfileResponse
+    {
+        public Tuple<int, string> IncorrectPassword { get; } = Tuple.Create(-2, "Uneli ste pogrešnu trenutnu lozinku!");
+        public Tuple<int, string> AlreadyExistsWithThisEmail { get; } = Tuple.Create(-1, "Već postoji korisnik sa ovom email adresom!");
+        public Tuple<int, string> SuccessfullyModifiedData { get; } = Tuple.Create(0, "Uspešno ste ažurirali vaše podatke!");
+    }
+}
