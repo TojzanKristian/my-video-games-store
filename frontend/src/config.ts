3876da6d1621f00288bc3da0f52dcd168d5d6605
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-12 10:07:48 +0200

    Add PayPal ID

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
 export const googleClientId = '641535140061-apk6fctf2b7rhsnihgsvdg1v9846l7oc.apps.googleusercontent.com';
+export const payPalId = 'AXcKOZmPHQB9M1XshLGwz9kffTh_oM8W9B9tgEmO_uXXQK85sNgcv2BOx_1BKvRzMowXxcSY7OQCjDF6';
 export const SERVER_URL = 'https://localhost:7130';
