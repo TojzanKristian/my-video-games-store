ee76f0a369e550223014b121e3d16b1eb42c6bb0
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-13 09:58:43 +0200

    GamesDbContext

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Database/Games/GamesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVideoGamesStoreAPI.Models;
+
+namespace MyVideoGamesStoreAPI.Database.Games
+{
+    public class GamesDbContext : DbContext
+    {
+        public DbSet<VideoGame> Games { get; set; }
+
+        public GamesDbContext(DbContextOptions<GamesDbContext> options) : base(options)
+        {
+        }
+    }
+}
