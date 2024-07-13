c6cd5275d0b940d3f5bddfb315b5b76255babf83
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-13 09:57:07 +0200

    LoginService

--- frontend/src/services/Login/LoginService.ts
+++ frontend/src/services/Login/LoginService.ts
@@ -1,9 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios, { AxiosError } from 'axios';
-import { SERVER_URL } from '../../config'
+import { SERVER_URL } from '../../config';
+import { ILoginModel } from '../../interfaces/ILoginModel';
 
 class LoginService {
-    async loginUser(logindata: any) {
+    async loginUser(logindata: ILoginModel) {
         try {
             const response = await axios.post(`${SERVER_URL}/api/users/login`, {
                 email: logindata.email,
