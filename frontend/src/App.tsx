a5916a3d9e93831f7ae9ccaae8b10aafb7e0032d
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-15 10:16:29 +0200

    Route settings and CartProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Registration from './pages/Registration/Registration';
 import Login from './pages/Login/Login';
@@ -11,6 +11,10 @@ import AllUsers from './pages/All users admin/AllUsers';
 import AllPurchases from './pages/All purchases admin/AllPurchases';
 import AddNewGame from './pages/Add new game admin/AddNewGame';
 import AllGames from './pages/All games admin/AllGames';
+import Cart from './pages/Cart/Cart';
+import Home from './pages/Home/Home';
+import VideoGameDetails from './pages/Video game details/VideoGameDetails';
+import { CartProvider } from './components/Cart context/CartContext';
 
 const App: React.FC = () => {
   // Funkcija za podešavanje za google autentifikaciju
@@ -25,19 +29,25 @@ const App: React.FC = () => {
   });
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/my-games" element={<MyGames />} />
-        <Route path="/all-users" element={<AllUsers />} />
-        <Route path="/all-purchases" element={<AllPurchases />} />
-        <Route path="/all-games" element={<AllGames />} />
-        <Route path="/add-new-game" element={<AddNewGame />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/tk-store" />} />
+          <Route path="/tk-store" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="/my-games" element={<MyGames />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/all-purchases" element={<AllPurchases />} />
+          <Route path="/all-games" element={<AllGames />} />
+          <Route path="/add-new-game" element={<AddNewGame />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/video-game-details/:name" element={<VideoGameDetails />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
