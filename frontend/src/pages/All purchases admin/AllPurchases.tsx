2011945604522ce37a082ab5408c195495aec2c5
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-16 10:03:22 +0200

    AllPurchases

--- frontend/src/pages/All purchases admin/AllPurchases.tsx
+++ frontend/src/pages/All purchases admin/AllPurchases.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AllPurchases.css';
 import { FaSignOutAlt } from 'react-icons/fa';
 import { useNavigate } from "react-router-dom";
 import NavBarAllPurchases from '../../components/NavBar/NavBarAllPurchases';
+import { IPurchase } from "../../interfaces/IPurchase";
+import AdminService from "../../services/Admin/AdminService";
 
 const AllPurchases: React.FC = () => {
 
     const redirection = useNavigate();
+    const [allPurchases, setAllPurchases] = useState<IPurchase[]>([]);
 
+    useEffect(() => {
+        // Zaštita stranice
+        const token = localStorage.getItem('token');
+        if (!token) {
+            redirection('/login');
+        }
+
+        // Funkcija za dobavljanje podataka o svim kupovinama u sistemu sa servera
+        const fetchAllPurchasesData = async () => {
+            try {
+                const response = await AdminService.getAllPurchases();
+                if (response !== undefined) {
+                    if (response.email === 'kristiantojzan@gmail.com') {
+                        setAllPurchases(response.data);
+                    }
+                    else {
+                        redirection('/login');
+                    }
+                }
+            } catch (error) {
+                console.error('Došlo je do greške: ', error);
+            }
+        };
+        fetchAllPurchasesData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    // Funkcija za obradu odjave sa sistema
     const logOut = async () => {
-        redirection('/login');
+        try {
+            localStorage.removeItem('token');
+            redirection('/login');
+        } catch (error) {
+            console.error('Došlo je do greške:', error);
+        }
     }
 
     return (
@@ -27,55 +63,21 @@ const AllPurchases: React.FC = () => {
                 <table className="table table-dark table-hover">
                     <thead>
                         <tr>
-                            <th scope="col" className='thStyle'>Slika</th>
-                            <th scope="col" className='thStyle'>Naziv igrice</th>
+                            <th scope="col" className='thStyle'>Spisak kupljenih igrica</th>
                             <th scope="col" className='thStyle'>Cena</th>
-                            <th scope="col" className='thStyle'>Ključ</th>
                             <th scope="col" className='thStyle'>Korisničko ime kupca</th>
                             <th scope="col" className='thStyle'>Datum kupovine</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="tdStyle"><img src="/cs2.jpg" alt="image" className="image-style " /></td>
-                            <td className="tdStyle">Counter Strike 2</td>
-                            <td className="tdStyle">50</td>
-                            <td className="tdStyle">ADA7ADAAHA6751AD</td>
-                            <td className="tdStyle">marko23</td>
-                            <td className="tdStyle">05.07.2024</td>
-                        </tr>
-                        <tr>
-                            <td className="tdStyle"><img src="/cs2.jpg" alt="image" className="image-style " /></td>
-                            <td className="tdStyle">Counter Strike 2</td>
-                            <td className="tdStyle">50</td>
-                            <td className="tdStyle">ADA7ADAAHA6751AD</td>
-                            <td className="tdStyle">marko23</td>
-                            <td className="tdStyle">05.07.2024</td>
-                        </tr>
-                        <tr>
-                            <td className="tdStyle"><img src="/cs2.jpg" alt="image" className="image-style " /></td>
-                            <td className="tdStyle">Counter Strike 2</td>
-                            <td className="tdStyle">50</td>
-                            <td className="tdStyle">ADA7ADAAHA6751AD</td>
-                            <td className="tdStyle">marko23</td>
-                            <td className="tdStyle">05.07.2024</td>
-                        </tr>
-                        <tr>
-                            <td className="tdStyle"><img src="/cs2.jpg" alt="image" className="image-style " /></td>
-                            <td className="tdStyle">Counter Strike 2</td>
-                            <td className="tdStyle">50</td>
-                            <td className="tdStyle">ADA7ADAAHA6751AD</td>
-                            <td className="tdStyle">marko23</td>
-                            <td className="tdStyle">05.07.2024</td>
-                        </tr>
-                        <tr>
-                            <td className="tdStyle"><img src="/cs2.jpg" alt="image" className="image-style " /></td>
-                            <td className="tdStyle">Counter Strike 2</td>
-                            <td className="tdStyle">50</td>
-                            <td className="tdStyle">ADA7ADAAHA6751AD</td>
-                            <td className="tdStyle">marko23</td>
-                            <td className="tdStyle">05.07.2024</td>
-                        </tr>
+                        {allPurchases.map((purchase, index) => (
+                            <tr key={index}>
+                                <td className="tdStyle">{purchase.listOfPurchasedGames}</td>
+                                <td className="tdStyle">{purchase.price}</td>
+                                <td className="tdStyle">{purchase.buyerUsername}</td>
+                                <td className="tdStyle">{purchase.dateOfPurchase}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </main>
