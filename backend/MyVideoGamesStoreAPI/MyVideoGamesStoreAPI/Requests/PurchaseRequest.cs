9a187aaa44dc8ac1dfabe2b70bd5234020dd4f1b
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-17 09:52:05 +0200

    PurchaseRequest model for purchases

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Requests/PurchaseRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MyVideoGamesStoreAPI.Requests
+{
+    public class PurchaseRequest
+    {
+        public string? ListOfPurchasedGames { get; set; }
+        public string? Price { get; set; }
+    }
+}
