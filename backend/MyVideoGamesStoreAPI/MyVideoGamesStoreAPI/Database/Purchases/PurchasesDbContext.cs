5f9cda9e1ba498463495870b5b860272513f0e44
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-17 09:39:59 +0200

    PurchasesDbContext

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Database/Purchases/PurchasesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVideoGamesStoreAPI.Models;
+
+namespace MyVideoGamesStoreAPI.Database.Purchases
+{
+    public class PurchasesDbContext : DbContext
+    {
+        public DbSet<Purchase> Purchases { get; set; }
+
+        public PurchasesDbContext(DbContextOptions<PurchasesDbContext> options) : base(options)
+        {
+        }
+    }
+}
