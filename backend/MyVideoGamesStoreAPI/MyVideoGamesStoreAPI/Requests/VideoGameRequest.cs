f5f2879d02832c17ecd985a3d05ad509c73695cc
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-17 09:50:42 +0200

    VideoGameRequest model for adding new video game by admin

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Requests/VideoGameRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MyVideoGamesStoreAPI.Requests
+{
+    public class VideoGameRequest
+    {
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Price { get; set; }
+        public string? YoutubeLink { get; set; }
+        public IFormFile? Image { get; set; }
+    }
+}
