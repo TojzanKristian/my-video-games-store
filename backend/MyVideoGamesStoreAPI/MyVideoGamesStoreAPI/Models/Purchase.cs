094b3f512c38ca7cd949cbd1902c9d7758794da4
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-17 09:43:32 +0200

    Purchase model

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Models/Purchase.cs
@@ -0,0 +1,19 @@
+﻿namespace MyVideoGamesStoreAPI.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string? ListOfPurchasedGames { get; set; }
+        public string? Price { get; set; }
+        public string? BuyerUsername { get; set; }
+        public string? DateOfPurchase { get; set; }
+
+        public Purchase(string? listOfPurchasedGames, string? price, string? buyerUsername, string? dateOfPurchase)
+        {
+            ListOfPurchasedGames = listOfPurchasedGames;
+            Price = price;
+            BuyerUsername = buyerUsername;
+            DateOfPurchase = dateOfPurchase;
+        }
+    }
+}
