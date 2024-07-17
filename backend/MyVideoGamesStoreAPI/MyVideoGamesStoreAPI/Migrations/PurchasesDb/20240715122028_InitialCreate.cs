3956a81e301aa471d6fc0b9c2a1a5e9a75f07972
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-17 09:47:28 +0200

    Add purchases migrations

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Migrations/PurchasesDb/20240715122028_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyVideoGamesStoreAPI.Migrations.PurchasesDb
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Purchases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ListOfPurchasedGames = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuyerUsername = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfPurchase = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchases", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Purchases");
+        }
+    }
+}
