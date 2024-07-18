c01e46b6c20569e0c071d848588ccfe73cfc209d
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-18 11:52:06 +0200

    PurchaseDto

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/DTOs/PurchaseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MyVideoGamesStoreAPI.DTOs
+{
+    public class PurchaseDto
+    {
+        public string? ListOfPurchasedGames { get; set; }
+        public string? Price { get; set; }
+        public string? BuyerUsername { get; set; }
+        public string? DateOfPurchase { get; set; }
+    }
+}
