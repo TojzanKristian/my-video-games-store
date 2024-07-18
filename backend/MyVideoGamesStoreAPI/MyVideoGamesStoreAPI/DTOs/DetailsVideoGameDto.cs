6fb3c4c10dc6567a815963f0aad326c9d31659ab
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-18 11:51:50 +0200

    VideoGameDto

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/DTOs/DetailsVideoGameDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MyVideoGamesStoreAPI.DTOs
+{
+    public class DetailsVideoGameDto
+    {
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Price { get; set; }
+        public string? YoutubeLink { get; set; }
+    }
+}
---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/DTOs/HomePageVideoGameDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyVideoGamesStoreAPI.DTOs
+{
+    public class HomePageVideoGameDto
+    {
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Price { get; set; }
+    }
+}
