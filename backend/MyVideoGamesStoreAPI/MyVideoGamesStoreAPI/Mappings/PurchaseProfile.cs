5bbf3b8859d8a20e86fb81fc446b8baf8bf67eb3
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-18 11:53:11 +0200

    PurchaseProfile

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Mappings/PurchaseProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MyVideoGamesStoreAPI.DTOs;
+using MyVideoGamesStoreAPI.Models;
+
+namespace MyVideoGamesStoreAPI.Mappings
+{
+    public class PurchaseProfile : Profile
+    {
+        public PurchaseProfile()
+        {
+            CreateMap<Purchase, PurchaseDto>();
+        }
+    }
+}
