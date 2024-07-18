abadd651c0774ea83e339cbb8f2a94be6f2a551a
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-18 11:52:54 +0200

    VideoGameProfile

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Mappings/VideoGameProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MyVideoGamesStoreAPI.DTOs;
+using MyVideoGamesStoreAPI.Models;
+
+namespace MyVideoGamesStoreAPI.Mappings
+{
+    public class VideoGameProfile : Profile
+    {
+        public VideoGameProfile()
+        {
+            CreateMap<VideoGame, HomePageVideoGameDto>();
+            CreateMap<VideoGame, DetailsVideoGameDto>();
+        }
+    }
+}
