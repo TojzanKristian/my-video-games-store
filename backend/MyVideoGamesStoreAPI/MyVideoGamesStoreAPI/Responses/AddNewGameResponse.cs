1459eb73e40b06915ec74d3d982599233934a927
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-18 11:51:15 +0200

    AddNewGameResponse

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Responses/AddNewGameResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MyVideoGamesStoreAPI.Responses
+{
+    public class AddNewGameResponse
+    {
+        public Tuple<int, string> VideoGameAlreadyExists { get; } = Tuple.Create(-1, "Igrica sa ovim nazivom već postoji u bazi podataka!");
+        public Tuple<int, string> SuccessfullyAdded { get; } = Tuple.Create(0, "Igrica je uspešno dodata u sistem!");
+    }
+}
