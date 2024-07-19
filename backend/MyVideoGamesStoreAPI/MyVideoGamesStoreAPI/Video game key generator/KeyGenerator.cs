b766984e31c31c996ac4130c9f2ebb202883d035
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-19 12:08:14 +0200

    KeyGenerator for generating random Steam activation key

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Video game key generator/KeyGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace MyVideoGamesStoreAPI.Video_game_key_generator
+{
+    public class KeyGenerator
+    {
+        /// <summary>
+        /// Generates a Steam activation key.
+        /// Generisanje Steam aktivacionog ključa.
+        /// </summary>
+        /// <returns>A string representing the generated Steam activation key.</returns>
+        public static string GenerateKey()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const int sections = 3;
+            const int charsPerSection = 5;
+            var random = new Random();
+            var keyBuilder = new StringBuilder();
+
+            for (int i = 0; i < sections; i++)
+            {
+                for (int j = 0; j < charsPerSection; j++)
+                {
+                    keyBuilder.Append(chars[random.Next(chars.Length)]);
+                }
+                if (i < sections - 1)
+                {
+                    keyBuilder.Append('-');
+                }
+            }
+
+            return keyBuilder.ToString();
+        }
+    }
+}
