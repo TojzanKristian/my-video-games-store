92cbb0d59731d4e71f1d5363d8b70be9d57dbb2a
Tojzán Krisztián <kristiantojzan@gmail.com>
2024-07-23 09:56:51 +0200

    VideoGameDetails model

---
+++ backend/MyVideoGamesStoreAPI/MyVideoGamesStoreAPI/Models/VideoGameDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace MyVideoGamesStoreAPI.Models
+{
+    public class VideoGameDetails
+    {
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public string? Price { get; set; }
+        public string? YoutubeLink { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public VideoGameDetails(string? name, string? category, string? price, string? youtubeLink, string? imageUrl)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+            YoutubeLink = youtubeLink;
+            ImageUrl = imageUrl;
+        }
+    }
+}
